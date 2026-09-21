b7cf092deca455b1775b298308902bba352c4318
nikiluda <zhanlin_nikita27@mail.ru>
2026-09-21 14:56:15 +0600

    feat: add generateToken

--- src/main/java/com/zhanlin/library_management_system/security/LibraryUserDetails.java
+++ src/main/java/com/zhanlin/library_management_system/security/LibraryUserDetails.java
@@ -53,4 +53,8 @@ public class LibraryUserDetails implements UserDetails {
     public boolean isEnabled() {
         return user.isEnabled();
     }
+
+    public LibraryUser getUser() {
+        return user;
+    }
 }
--- src/main/java/com/zhanlin/library_management_system/security/service/JwtService.java
+++ src/main/java/com/zhanlin/library_management_system/security/service/JwtService.java
@@ -1,11 +1,16 @@
 package com.zhanlin.library_management_system.security.service;
 
+import com.zhanlin.library_management_system.security.LibraryUserDetails;
 import com.zhanlin.library_management_system.security.config.JwtProperties;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Service
 public class JwtService {
@@ -20,6 +25,32 @@ public class JwtService {
                 jwtProperties.getSecret()
                         .getBytes(StandardCharsets.UTF_8)
         );
+    }
+
+
+    public String generateToken(Authentication authentication) {
+
+        LibraryUserDetails userDetails = (LibraryUserDetails) authentication.getPrincipal();
+
+        Long userId = userDetails.getUser().getId();
+
+        return Jwts.builder()
+                .subject(userId.toString())
+                .claim(
+                        "roles",
+                        userDetails.getAuthorities()
+                                .stream()
+                                .map(GrantedAuthority::getAuthority)
+                                .toList()
+                )
+                .issuedAt(new Date())
+                .expiration(new Date(
+                        System.currentTimeMillis() + jwtProperties.getExpiration()
+                        )
+                )
+                .signWith(secretKey)
+                .compact();
+
     }
 
 }
